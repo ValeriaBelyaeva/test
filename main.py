8521fbf3d403819a34472f1d5466ccc3628546ea
ValeriaBelyaeva <iresu1409@yandex.ru>
2023-03-30 12:38:00 +0300

    test commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import random, time, sys
+from pygame.locals import *
+
+pygame.init()
+from settings import *
+
+screen = pygame.display.set_mode((window_w, window_h))
+# pygame.display.set_mode((window_w, window_h))
+pygame.display.set_caption('Тетрис')
+desk = [[4 for i in range(8)] for i in range(11)]
+
+flRunning = True
+pause=0
+while flRunning:
+    for j in range(11):
+        for i in range(8):
+            color = colors[desk[j][i]]
+            pygame.draw.rect(screen,color,(50+i*int(side*1.2),150+j*int(side*1.2),side,side))
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            flRunning = False
+
+        if event.type == KEYUP:
+            if event.key == K_SPACE:
+                if pause==0:
+                    pause = 1
+
+                    screen.fill(colors[0])
+                #else: pause.fill(())
+
+    fps_clock = pygame.time.Clock()
